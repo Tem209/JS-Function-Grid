e9e805a99db142b7f20ac5057fb05810917530b3
Tembani <tembani437@gmail.com>
2017-01-11 21:20:31 +0000

    I made pixel art

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //Write your function calls below this line
+setColor(0, 0, "purple");
+setColor(0, 9, "purple");
+setColor(9, 0, "purple");
+setColor(9, 9, "purple");
+setColor(2, 1, "black");
+setColor(2, 4, "black");
+setColor(3, 1, "black");
+setColor(3, 4, "black");
+setColor(6, 0, "purple");
+setColor(7, 1, "purple");
+setColor(7, 2, "purple");
+setColor(7, 3, "purple");
+setColor(7, 4, "purple");
+setColor(2, 7, "orange");
+setColor(3, 6, "orange");
+setColor(4, 6, "orange");
+setColor(5, 7, "orange");
